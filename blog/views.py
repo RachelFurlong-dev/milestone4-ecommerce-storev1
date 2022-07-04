c1506879556ac4360c1e99a521bebf85cbe23649
RachelFurlong-dev <rachel.furlong@ekcgroup.ac.uk>
2022-07-04 09:35:04 +0000

    Added blog link to main nav

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,4 @@ from blog.models import Post
 # Create your views here.
 
 def view_blog(request):
-    """ A view that renders the blog contents page """
-
     return render(request, 'blog/blog.html')
